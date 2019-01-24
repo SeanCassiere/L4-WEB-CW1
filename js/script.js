4038a147e4f6506e9d4604a0c69bd9c903987a6a
SeanCassiere <33615041+SeanCassiere@users.noreply.github.com>
2019-01-24 23:43:13 +0530

    V0.3
    Changes made to the CSS3 Grid implementation.

--- js/script.js
+++ js/script.js
@@ -2,13 +2,13 @@ function increaseFontSize() {
     txt = document.getElementById('content');
     style = window.getComputedStyle(txt, null).getPropertyValue('font-size');
     currentSize = parseFloat(style);
-    txt.style.fontSize = (currentSize + 1) + 'px';
+    txt.style.fontSize = (currentSize + 2) + 'px';
 }
 function decreaseFontSize() {
 	txt = document.getElementById('content');
     style = window.getComputedStyle(txt, null).getPropertyValue('font-size');
     currentSize = parseFloat(style);
-    txt.style.fontSize = (currentSize - 1) + 'px';
+    txt.style.fontSize = (currentSize - 2) + 'px';
 }
 
 document.getElementById("decreaseFont").addEventListener("click",function(){decreaseFontSize();});
