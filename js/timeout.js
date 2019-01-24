8424545dec4c7f9c0b27ba702ee99f704d9a7d03
SeanCassiere <33615041+SeanCassiere@users.noreply.github.com>
2019-01-25 00:43:54 +0530

    V0.3.0.1
    Moved Timeout Javascript function to an external file, js/timeout.js.

---
+++ js/timeout.js
@@ -0,0 +1,6 @@
+//setTimeout(function(){window.location.href = 'home.html';}, 4000);
+//var myTimer;
+
+document.getElementById("content").addEventListener("load",setTimeout(function(){window.location.href = 'home.html';}, 4000));
+//document.getElementById("restartTimer").addEventListener("click",setTimeout(function(){window.location.href = 'home.html';}, 4000));
+//document.getElementById("pauseTimer").addEventListener("click",setTimeout(function(){window.location.href = 'home.html';}, 4000));
